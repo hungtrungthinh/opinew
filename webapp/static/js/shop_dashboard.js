ca040f1903cd774c281063095088ea351eb076c4
Daniel Tsvetkov <danieltcv@gmail.com>
2015-11-22 16:57:31 +0000

    fix #55

--- webapp/static/js/shop_dashboard.js
+++ webapp/static/js/shop_dashboard.js
@@ -13,3 +13,35 @@ for (var i = 0; i < pages.length; i++) {
   var page = pages[i];
   getPage(page);
 }
+
+$('#shop-form').bind('submit', function (e) {
+  var $form = $(this);
+  var formData = {};
+
+  $form.find(".form-serialize").each(function () {
+    formData[this.name] = $(this).val();
+  });
+  $('#ajax-status').hide()
+
+  $.ajax({
+    type: $form.attr('method'),
+    url: $form.attr('action'),
+    data: JSON.stringify(formData),
+    contentType: 'application/json'
+  }).done(function (r) {
+    $('#product-review').hide();
+    $('#ajax-status')
+        .removeClass('alert-danger')
+        .addClass('alert-success')
+        .html('<p>Shop changes saved!</p>')
+        .slideDown();
+  }).fail(function (r) {
+    var errors = JSON.stringify(r.responseJSON.validation_errors) || JSON.stringify(r.responseJSON.message);
+    $('#ajax-status')
+        .addClass('alert-danger')
+        .html('<p><strong>Something went wrong</strong>: ' + errors + '</p>')
+        .slideDown();
+
+  });
+  return false;
+});
