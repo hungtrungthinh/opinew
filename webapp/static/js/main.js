1692ff8a83ad4a759856ed0c880a56a188bf658e
Daniel Tsvetkov <danieltcv@gmail.com>
2015-11-30 20:36:12 +0000

    fix regression of switching tabs on shop dashboard and disable experimental questions for now

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -52,11 +52,11 @@ $.fn.serializeObject = function () {
 // Javascript to enable link to tab
 var url = document.location.toString();
 if (url.match('#')) {
-  $('.nav-pills a[href=#' + url.split('#')[1] + ']').tab('show');
+  $('.nav-tabs a[href=#' + url.split('#')[1] + ']').tab('show');
 }
 
 // Change hash for page-reload
-$('.nav-pills a').on('shown.bs.tab', function (e) {
+$('.nav-tabs a').on('shown.bs.tab', function (e) {
   window.location.hash = e.target.hash;
 });
 
