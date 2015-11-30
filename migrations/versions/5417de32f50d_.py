8fee908d764680af7a700dca9d478c35e83e1716
Daniel Tsvetkov <danieltcv@gmail.com>
2015-11-30 18:10:41 +0000

    merged emails and questions

---
+++ migrations/versions/5417de32f50d_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 5417de32f50d
+Revises: ('2bdd21038fa7', '2d09e604b03a')
+Create Date: 2015-11-30 18:09:34.691587
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '5417de32f50d'
+down_revision = ('2bdd21038fa7', '2d09e604b03a')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
